db9ac99c31dac08f67988e89192bfc0d7d1d8f18
Levi Cook <levicook@gmail.com>
2014-08-29 17:39:11 -0600

    meh

---
+++ Events/Workshops/Go-Primer/code/if-statement-01-todo.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// START OMIT
+func max(i, j int) int {
+	return 0
+}
+
+// END OMIT
+
+func Test_max(t *testing.T) {
+	assert.Equal(t, 1, max(0, 1))
+}
+
+func main() {
+	var tests []testing.InternalTest
+	tests = append(tests, testing.InternalTest{Name: "Test_max", F: Test_max})
+	testing.Main(func(pat, str string) (bool, error) { return true, nil }, tests, nil, nil)
+}
