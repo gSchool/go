8a454158aef577cad37ea0ad637826155886ef62
Cory LaNou <cory@lanou.com>
2014-10-14 11:00:59 -0700

    show more of example

--- Exercises/Command-Line-Programming/code/cat/usage.go
+++ Exercises/Command-Line-Programming/code/cat/usage.go
@@ -6,19 +6,11 @@ import (
 	"os"
 )
 
-func init() {
-	os.Args = append(os.Args, "-h")
-}
-
 func main() {
-	//START OMIT
 	flag.Usage = func() {
 		fmt.Printf("Usage of %s:\n", os.Args[0])
 		fmt.Printf("	cat file1 file2 ...\n")
 		flag.PrintDefaults()
 	}
-
 	flag.Parse()
-	//END OMIT
-
 }
